4d02f0f3cc51adf1a411ab941b0d006759efd2d8
Pawel Dudek <pawelfree@wp.pl>
2018-03-07 17:35:50 +0100

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout'
 
 import 'hammerjs'
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     FlexLayoutModule
   ],
   providers: [],
