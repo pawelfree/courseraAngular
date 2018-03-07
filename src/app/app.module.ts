76af41a9aa8e3a1494e755597ba3d3453fef1989
Pawel Dudek <pawelfree@wp.pl>
2018-03-07 03:21:35 +0100

    Configure app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+import 'hammerjs'
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
