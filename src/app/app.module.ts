5537de5e8e4900f7a3c61f47d950d06d3954bd35
Pawel Dudek <pawelfree@wp.pl>
2018-03-11 08:42:07 +0100

    Routing Works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,32 @@ import { FooterComponent } from './footer/footer.component';
 
 import { DishService } from './services/dish.service';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent },
+  {path: 'menu', component: MenuComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+import { AppRoutingModule } from './app-routing/app-routing.module'
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +55,8 @@ import { DishService } from './services/dish.service';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
